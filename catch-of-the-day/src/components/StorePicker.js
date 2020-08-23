08bfaf559d53ff7c2a9bfe5d35eb58daa581d67d
nathan <nathan.chalot@gmail.com>
2020-08-23 09:10:54 +0200

    Get input value via a ref in StorePicker component
    note that in the tutorial he's using XXXX.value.value to access the input
    value, but now the react doc says to use XXXX.current.value to access that.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+  storeInput = React.createRef();
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this.storeInput.current.value);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" ref={this.storeInput}/>
         <button type="submit">Visit Store</button>
       </form>
     );
