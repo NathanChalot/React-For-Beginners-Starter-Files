6b5620dd266aa61725e6004c5d466483bb5321aa
nathan <nathan.chalot@gmail.com>
2020-08-23 19:47:19 +0200

    Compute and display order total in Order

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 class Order extends React.Component {
   render() {
+    const orderIds = Object.keys(this.props.orders);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.orders[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    },0);
     return (
-      <div className="order">here goes Order</div>
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <div className="total">
+          Total:
+          {total}
+        </div>
+      </div>
     );
   }
 }
